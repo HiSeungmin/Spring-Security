789cbd01d5e67c161ba95b3f55eb03bf94b5cca4
HiSeungmin <osminosmin7180@gmail.com>
2024-12-31 13:09:29 +0900

    Setting Spring Security

--- src/main/java/fast/campus/spring_security/controller/HelloController.java
+++ src/main/java/fast/campus/spring_security/controller/HelloController.java
@@ -1,4 +1,13 @@
 package fast.campus.spring_security.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/api/v1/hello")
+    public String hello() {
+        return "Hello, Spring Security!";
+    }
 }
